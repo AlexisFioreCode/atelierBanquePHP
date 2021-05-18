e91826a2499e7f72005665bf4c39cb613112e135
AlexisFioreCode <alexis.fiorentino.pro@gmail.com>
2021-05-18 11:01:36 +0200

    login/logout

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
     require "model/accounts.php";
     include "layout/header.php";
     $accounts = get_accounts();
+    session_start();
+    if(!isset($_SESSION["user"])) {
+      header('Location: login.php');  
+      exit();
+    }
 ?>
 
 <h2>Vos comptes en banque</h2>
-
+<a href="logout.php" class="btn btn-dark text-white">Déconnexion</a>
 <div class="row">
     <?php foreach ($accounts as $index => $account): ?>
         <div class="col-6 col-md-4">
--- login.php
+++ login.php
@@ -1,8 +1,36 @@
 <?php 
     include "layout/header.php";
+    $id = "jean";
+    $mdp = "michel"; 
+    if(!empty($_POST)) {
+        $login = $_POST;
+        foreach ($login as $key => $value) {
+            $login[$key] = htmlspecialchars($value);
+        }
+        if(($login['username'] === $id) && ($login['password'] === $mdp)) {
+            session_start();
+            $_SESSION["user"] = $value;
+            header('Location: index.php');
+            exit();
+        }
+    }
+    
 ?>
 
-
+<h2>Se connecter</h2>
+<form action="login.php" method="post">
+    <input name="username" class="form-control my-2" type="text">
+    <input name="password" class="form-control my-2" type="password">
+    <input hrefname="login" class="form-control btn btn-dark text-white my-2" type="submit" value="Login">  
+    <?php if(!empty($login)): 
+        if (($login['username'] !== $id) && ($login['password'] !== $mdp)): ?>
+            <div class="alert alert-danger">
+                <p> Identifiant ou mot de passe incorrects veuillez réessayer</p>
+            </div>
+       <?php endif ?>
+  <?php  endif ?>
+    
+</form>
 
 
 
